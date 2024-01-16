c30bdee8438f96b29365587414a078c50f7550ff
Andrey Fomkin <fomkin-opt@yandex.ru>
2024-01-16 22:02:28 +0300

    Задача 1

---
+++ Task2.cs
@@ -0,0 +1,27 @@
+int Infiniti (int number, string q)
+{
+    
+    while(true){
+        Console.Write("Введите текст: ");
+        string text = Console.ReadLine();
+        if (text=="q")
+        {
+            break;
+        }
+    }
+    if (int.TryParse(text, out number))
+    {
+         Console.Write("Введённая строчка состоит только из цифр: ");  
+    }
+    int sum = 0;
+    while (number>0)
+    {
+        int sum = firstnumber + number/10;
+    } 
+    if (sum%2==0)
+    {
+        break;
+    }
+
+    }
+
