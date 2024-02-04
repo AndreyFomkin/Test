c51197f65f69ab0e975c3c00a33a3ae3cf0355bc
Andrey Fomkin <fomkin-opt@yandex.ru>
2024-02-04 22:58:40 +0300

    Задача M&N

---
+++ M&N.cs
@@ -0,0 +1,19 @@
+Console.Write("Введите число: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int start = M;
+int end = N;
+void ShowNumbers(int start, int end)
+{
+    if (start == end || start > end)
+{ Console.Write(start); 
+      return;
+    }
+    
+    
+    Console.Write(start + " "); 
+    ShowNumbers(start + 1, end);
+}
+ShowNumbers(start, N); 
+
